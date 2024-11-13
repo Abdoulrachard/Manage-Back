563309a7c32615991b8033005b0c76e07865eda1
Abdoulrachard <abdoulrachard@gmail.com>
2024-11-13 11:46:42 +0100

    updated workflow to build assets in prod

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="icon" href="{{ asset('/favicon.ico') }}" type="image/x-icon">
+        <link rel="icon" href="/public/favicon.ico" type="image/x-icon">
 
         
         <title>@yield('title') | CCHE</title>
